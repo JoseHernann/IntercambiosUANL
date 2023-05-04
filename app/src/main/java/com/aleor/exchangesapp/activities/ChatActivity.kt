71698d4beb92ebecdd854e1bb67ba478d9eaf0ba
Alejandra Orozco <104922128+fairyaleee@users.noreply.github.com>
2023-05-04 15:43:02 -0600

    Cambios en el bottom menu bar

--- app/src/main/java/com/aleor/exchangesapp/activities/ChatActivity.kt
+++ app/src/main/java/com/aleor/exchangesapp/activities/ChatActivity.kt
@@ -1,9 +1,11 @@
 package com.aleor.exchangesapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.aleor.exchangesapp.R
 import com.aleor.exchangesapp.databinding.ActivityChatBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ChatActivity : AppCompatActivity() {
 
@@ -14,5 +16,27 @@ class ChatActivity : AppCompatActivity() {
 
         binding = ActivityChatBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //Boton de regreso a Home
+        binding.backButton.setOnClickListener{
+            backToHome()
+        }
+
+        //Boton de regreso a Home
+        binding.rightButton.setOnClickListener{
+            goToMap()
+        }
     }
+
+    //Regresar a Home
+    private fun backToHome(){
+        val intent =  Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun goToMap(){
+        val intent =  Intent(this, MapActivity::class.java)
+        startActivity(intent)
+    }
+
 }
