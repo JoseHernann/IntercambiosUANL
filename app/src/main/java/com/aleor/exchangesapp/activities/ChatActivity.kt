15be5e828dde96ccf4b6e4a15b8e4fa62e5e0258
Alejandra Orozco <104922128+fairyaleee@users.noreply.github.com>
2023-05-16 21:37:59 -0600

    Chat :(

--- app/src/main/java/com/aleor/exchangesapp/activities/ChatActivity.kt
+++ app/src/main/java/com/aleor/exchangesapp/activities/ChatActivity.kt
@@ -3,29 +3,37 @@ package com.aleor.exchangesapp.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.aleor.exchangesapp.Fragments.ChatsFragment
+import com.aleor.exchangesapp.Fragments.SearchFragment
+import com.aleor.exchangesapp.Fragments.SettingsFragment
 import com.aleor.exchangesapp.R
-import com.aleor.exchangesapp.databinding.ActivityChatBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.tabs.TabLayout
 
 class ChatActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityChatBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
 
-        binding = ActivityChatBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val toolbar: Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = ""
 
-        //Boton de regreso a Home
-        binding.backButton.setOnClickListener{
-            backToHome()
-        }
+        val tabLayout: TabLayout = findViewById(R.id.tab_layout)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
 
-        //Boton de regreso a Home
-        binding.rightButton.setOnClickListener{
-            goToMap()
-        }
+        viewPagerAdapter.addFragment(ChatsFragment(),"Chats")
+        viewPagerAdapter.addFragment(SearchFragment(), "Buscar")
+        viewPagerAdapter.addFragment(SettingsFragment(), "Mi cuenta")
+
+        viewPager.adapter = viewPagerAdapter
+        tabLayout.setupWithViewPager(viewPager)
     }
 
     //Regresar a Home
@@ -39,4 +47,32 @@ class ChatActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    internal class ViewPagerAdapter(fragmentManager: FragmentManager) :
+        FragmentPagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT){ //Cambiar el constructor
+
+        private val fragments: ArrayList<Fragment>
+        private val titles: ArrayList<String>
+
+        init{
+            fragments = ArrayList<Fragment>()
+            titles = ArrayList<String>()
+        }
+
+        override fun getCount(): Int {
+            return fragments.size
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return fragments[position]
+        }
+
+        fun addFragment(fragment: Fragment, title: String){
+            fragments.add(fragment)
+            titles.add(title)
+        }
+
+        override fun getPageTitle(i: Int): CharSequence?{
+            return titles[i]
+        }
+    }
 }
--- app/src/main/java/com/aleor/exchangesapp/activities/MapActivity.kt
+++ app/src/main/java/com/aleor/exchangesapp/activities/MapActivity.kt
@@ -2,6 +2,7 @@ package com.aleor.exchangesapp.activities
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
@@ -10,6 +11,7 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.view.WindowManager
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
@@ -57,6 +59,10 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, Listener {
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.ACCESS_COARSE_LOCATION
         ))
+
+        //ONLCIK DE BOTONES
+        binding.btnConnect.setOnClickListener { connectUser() }
+        binding.btnDisconnect.setOnClickListener { disconnectUser() }
     }
 
     //Permisos para traer la ubicacion actual
@@ -65,11 +71,11 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, Listener {
             when{
                 permission.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
                     Log.d("LOCALIZACION", "Permiso concedido")
-                    easyWayLocation?.startLocation()
+//                    easyWayLocation?.startLocation()
                 }
                 permission.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) ->{
                     Log.d("LOCALIZACION", "Permiso concedido con limitacion")
-                    easyWayLocation?.startLocation()
+//                    easyWayLocation?.startLocation()
                 }
                 else -> {
                     Log.d("LOCALIZACION", "Permiso no concedido")
@@ -77,6 +83,34 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, Listener {
             }
         }
     }
+
+    //Desconectar Usuario
+    private fun disconnectUser(){
+        easyWayLocation?.endUpdates()
+        if(myLocationLatLng != null){
+            showButtonConnect()
+        }
+    }
+
+    private fun connectUser(){
+        easyWayLocation?.endUpdates()
+        easyWayLocation?.startLocation()
+            showButtonDisconnect()
+
+    }
+
+
+    //Funcion que mostrara el boton "DESCONECTARSE" y "CONECTARSE"
+    private fun showButtonConnect(){
+        binding.btnDisconnect.visibility = View.GONE
+        binding.btnConnect.visibility = View.VISIBLE
+    }
+
+    private fun showButtonDisconnect(){
+        binding.btnDisconnect.visibility = View.VISIBLE
+        binding.btnConnect.visibility = View.GONE
+    }
+
 
     //Marcador personalizado
 
@@ -128,7 +162,7 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, Listener {
     override fun onMapReady(map: GoogleMap) {
         googleMap = map
         googleMap?.uiSettings?.isZoomControlsEnabled = true
-        easyWayLocation?.startLocation() //El metodo solo se ejecuta una vez
+//        easyWayLocation?.startLocation() //El metodo solo se ejecuta una vez
 
         if (ActivityCompat.checkSelfPermission(
                 this,
@@ -142,6 +176,17 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, Listener {
             return
         }
         googleMap?.isMyLocationEnabled = false
+        try{
+            val success = googleMap?.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(this,R.raw.style)
+            )
+            if(!success!!){
+                Log.d("MAPAS", "No se pudo encontrar el estilo")
+            }
+
+        }catch(e: Resources.NotFoundException){
+            Log.d("MAPAS", "Error: ${e.toString()}")
+        }
     }
 
     override fun locationOn() {
